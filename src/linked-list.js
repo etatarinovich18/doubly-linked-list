f1766820166c6547d95b47b1aa2a6f144fd97696
Tatarinovich Ekaterina <etatarinovich18@gmail.com>
2017-01-27 21:27:27 +0200

    double-linked-list: implement all methods

--- src/linked-list.js
+++ src/linked-list.js
@@ -22,20 +22,30 @@ class LinkedList {
         }
 
         this.length++;
+        
+        return this;
     }
 
     head() {
-        return this._head.data;
+        if (this._head == null) {
+            return null;
+        } else {
+            return this._head.data;
+        }
     }
 
     tail() {
-        return this._tail.data;
+        if (this._tail == null) {
+            return null;
+        } else {
+            return this._tail.data;
+        }
     }
 
     at(index) {
         this.index = index;
         var current = this._head;
-        //If index > length !!!!!!!!!!!!!!!
+
         if (this.index == 0) {
             // return this._head.data;
         } else if (this.index == this.length - 1) {
@@ -54,20 +64,41 @@ class LinkedList {
     insertAt(index, data) {
         this.index = index;
         this.data = data;
+        var node = new Node(this.data);
         var current = this._head;
 
-        if (this.index == 0) {
-            this._head.data = this.data;
-        } else if (this.index == this.length - 1) {
-            this._tail.data = this.data;
+        if (this.length == 0) {
+            this._head = node;
+            this._tail = node;
+        } else if (this.index == 0) {
+            
+            this._head.prev = node;
+            node.next = this._head;
+            this._head = node;
+
+        } else if (this.index == this.length) {
+            
+            this._tail.next = node;
+            node.prev = this._tail;
+            this._tail = node;
+
         } else {
             var count = 0;
-            while (count < this.index) {
+            while (count < this.index - 1) {
                 current = current.next;
                 count++;
             }
-            current.data = this.data;
+            var currentNext = current.next;
+            
+            node.next = current.next;
+            currentNext.prev = node;
+            
+            current.next = node;
+            node.prev = current;
         }
+        this.length++;
+        
+        return this;
     }
 
     isEmpty() {
@@ -75,16 +106,77 @@ class LinkedList {
     }
 
     clear() {
-
+        this._head = null;
+        this._tail = null;
+        this.length = 0;
+        
+        return this;
     }
 
     deleteAt(index) {
+        this.index = index;
+        var current = this._head;
+        
+        if (this.index == 0) {
+            this._head = this._head.next;
+        } else if (this.index == this.length - 1) {
+            this._tail = this._tail.prev;
+        } else {
+            var count = 1;
+
+            while (count < this.index) {
+                current = current.next;
+                count++;
+            }
+
+            var beforeNodeToDelete = current.prev;
+            var afterNodeToDelete = current.next;
+
+            beforeNodeToDelete.next = current.next;
+            afterNodeToDelete.prev = current.prev;
+
+            current = null;
+        }
+
+        this.length--;
+
+        return this;
     }
 
     reverse() {
+
+        var current = this._head;
+
+        while (current != null) {
+
+            var temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+
+            current = current.prev;
+        }
+
+        var tempForHead = this._head;
+        this._head = this._tail;
+        this._tail = tempForHead;
+
+        return this;
     }
 
-    indexOf(data) {}
+    indexOf(data) {
+        this.data = data;
+        var current = this._head;
+        var result = -1;
+
+        for (var index = 0; index < this.length; index++) {
+            if (current.data == this.data) {
+                result = index;
+            }
+            current = current.next;
+        }
+
+        return result;
+    }
 }
 
 module.exports = LinkedList;
